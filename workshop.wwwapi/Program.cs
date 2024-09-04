9aae5e4d22b7c1c131a571d7dd3e1eb988a3949d
uerbzr <nigel.sibbert@gmail.com>
2024-09-04 19:51:59 +0100

    updated

--- workshop.wwwapi/Program.cs
+++ workshop.wwwapi/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddValidatorsFromAssemblyContaining(typeof(CarValidator));
+builder.Services.AddValidatorsFromAssemblyContaining(typeof(CarPostValidator));
 builder.Services.AddScoped<IRepository, CarRepository>();
 builder.Services.AddDbContext<DataContext>();
 var app = builder.Build();
--- workshop.wwwapi/Validators/CarPostValidator.cs
+++ workshop.wwwapi/Validators/CarPostValidator.cs
@@ -4,9 +4,9 @@ using workshop.wwwapi.ViewModels;
 
 namespace workshop.wwwapi.Validators
 {
-    public class CarValidator : AbstractValidator<CarPostModel>
+    public class CarPostValidator : AbstractValidator<CarPostModel>
     {
-        public CarValidator()
+        public CarPostValidator()
         {
             RuleFor(c => c.Make).MaximumLength(255).Must(c => c.Length==1).WithMessage("lengh must be 1!").NotEmpty(); // an unreasonable validation to show how this works!
             RuleFor(c => c.Model).MaximumLength(255).NotEmpty();
