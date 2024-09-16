76f699cdf70ce3a9a50f1eea9376e327e48d92d9
uerbzr <nigel.sibbert@gmail.com>
2024-09-16 07:56:00 +0100

    updated

--- workshop.wwwapi/Validators/CarPostValidator.cs
+++ workshop.wwwapi/Validators/CarPostValidator.cs
@@ -8,8 +8,10 @@ namespace workshop.wwwapi.Validators
     {
         public CarPostValidator()
         {
-            RuleFor(c => c.Make).MaximumLength(255).Must(c => c.Length==1).WithMessage("lengh must be 1!").NotEmpty(); // an unreasonable validation to show how this works!
+            
+            RuleFor(c => c.Make).MaximumLength(255).Must(c => c.Length>1).WithMessage("lengh must be > 1!").NotEmpty(); // an unreasonable validation to show how this works!
             RuleFor(c => c.Model).MaximumLength(255).NotEmpty();
+            RuleFor(c => c.DoorCount).Must(c => c > 0).WithMessage("Car must have at least 1 door!");
         }
     }
 }
